c5412d858d5ace6b77cc19bb86fc03bfade7392d
User <user@iMac-de-admin-4.local>
2015-11-18 13:06:47 +0100

    Add function traget.heat

--- app/components/target/target.directive.js
+++ app/components/target/target.directive.js
@@ -1,29 +1,34 @@
 angular
 	.module('robin.target', [])
-	.directive('targetDirective', targetDirective);
+	.directive('target', target);
 
-	function targetDirective(){
+	function target(){
 		return {
-		restrict: 'E',
-        templateUrl: 'app/components/target/target.html',
-        scope: true,
-		controller: function targetCtrl(){
-          	this.titre = 'directive target';
-          	this.zones=[];
-			this.zones.push({id:"zone0", pt:0, r:"400", stroke:"#fff", fill:"#fff"});
-			this.zones.push({id:"zone1", pt:1, r:"150", stroke:"#000", fill:"#fff"});
-			this.zones.push({id:"zone2", pt:2, r:"135", stroke:"#000", fill:"#fff"});
-			this.zones.push({id:"zone3", pt:3, r:"120", stroke:"#000", fill:"#43484D"});
-			this.zones.push({id:"zone4", pt:4, r:"105", stroke:"#fff", fill:"#43484D"});
-			this.zones.push({id:"zone5", pt:5, r:"90", stroke:"#000", fill:"#4BB5C1"});
-			this.zones.push({id:"zone6", pt:6, r:"75", stroke:"#000", fill:"#4BB5C1"});
-			this.zones.push({id:"zone7", pt:7, r:"60", stroke:"#000", fill:"#D15D5C"});
-			this.zones.push({id:"zone8", pt:8, r:"45", stroke:"#000", fill:"#D15D5C"});
-			this.zones.push({id:"zone9", pt:9, r:"30", stroke:"#000", fill:"#FCFBA9"});
-			this.zones.push({id:"zone10", pt:10, r:"15", stroke:"#000", fill:"#FCFBA9"})
-        	},
-        controllerAs: "targetCtrl",
-        
-    	};
-		
+			restrict: 'E',
+	        templateUrl: 'app/components/target/target.html',
+	        scope: true,
+			controller: function targetCtrl(){
+
+				var vm = this;
+	          	vm.titre = 'target none';
+	          	vm.zones=[];
+				vm.zones.push({id:"zone0", pt:0, r:"400", stroke:"#fff", fill:"#eee"});
+				vm.zones.push({id:"zone1", pt:1, r:"150", stroke:"#000", fill:"#fff"});
+				vm.zones.push({id:"zone2", pt:2, r:"135", stroke:"#000", fill:"#fff"});
+				vm.zones.push({id:"zone3", pt:3, r:"120", stroke:"#000", fill:"#43484D"});
+				vm.zones.push({id:"zone4", pt:4, r:"105", stroke:"#fff", fill:"#43484D"});
+				vm.zones.push({id:"zone5", pt:5, r:"90", stroke:"#000", fill:"#4BB5C1"});
+				vm.zones.push({id:"zone6", pt:6, r:"75", stroke:"#000", fill:"#4BB5C1"});
+				vm.zones.push({id:"zone7", pt:7, r:"60", stroke:"#000", fill:"#D15D5C"});
+				vm.zones.push({id:"zone8", pt:8, r:"45", stroke:"#000", fill:"#D15D5C"});
+				vm.zones.push({id:"zone9", pt:9, r:"30", stroke:"#000", fill:"#FCFBA9"});
+				vm.zones.push({id:"zone10", pt:10, r:"15", stroke:"#000", fill:"#FCFBA9"})
+	        	
+				vm.heat = function(){
+					vm.titre = "target heat";
+					console.log("klwjflwe");
+				}
+	        },
+	        controllerAs: "targetCtrl",
+        };	
 	}
